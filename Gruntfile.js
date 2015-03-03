c19a6833275dc67d5d376e4090c0a98e8fd60302
Edward Byne <ejbyne@gmail.com>
2015-03-03 15:44:42 +0000

    sets up file structure, basic server and Gruntfile/package.json

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+
+    jasmine_node: {
+      options: {
+        forceExit: true,
+      },
+      all: ['spec/']
+    },
+
+    jshint: {
+        src: []
+    },
+    
+    watch: {
+      scripts: {
+        files: [],
+        tasks: ['jasmine_node', 'jshint']
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jasmine_node', 'jshint']);
+
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/app/views/index.html')
+});
+
+server.listen(port, function(){
+  console.log('Server listening on port ' + port);
+});
+
+module.exports = server;
